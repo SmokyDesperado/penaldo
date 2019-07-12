16697722369e38e80b8c6695b02e21be3d04a4a6
SmokyDesperado <desperadosmoky@gmail.com>
2019-07-12 09:48:41 +0200

    scripting updated
    simple aiming and shooting implemented

---
+++ Assets/Scripts/AimingControl.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimingControl : MonoBehaviour
+{
+
+public float PosX = 0.0f;
+public float PosY = 0.0f;
+public float PosZ = 0.0f;
+
+void start()
+{
+
+}
+
+// void FixedUpdate()
+// {
+//   if (Input.GetMouseButtonDown(0)) {
+//     getMousePosition();
+//   }
+// }
+
+void OnMouseDown()
+{
+  // float PosX = 0.0f;
+  // float PosY = 0.0f;
+  // float PosZ = 0.0f;
+
+  // Debug.Log("PosX: " + PosX + " | PosY: " + PosY + " | PosZ: " + PosZ);
+
+  Vector3 p = Input.mousePosition;
+  p.z = -3.635868f;
+  Vector3 Pos = Camera.main.ScreenToWorldPoint(p);
+  SetAimingPosition(Pos.x, Pos.y, Pos.z);
+  Debug.Log("PosX: " + PosX + " | PosY: " + PosY + " | PosZ: " + PosZ);
+}
+
+void SetAimingPosition(float AimingPosX, float AimingPosY, float AimingPosZ)
+{
+  PosX = AimingPosX;
+  PosY = AimingPosY;
+  PosZ = AimingPosZ;
+}
+
+public float getPosX()
+{
+  return PosX;
+}
+
+public float getPosY()
+{
+  return PosY;
+}
+
+public float getPosZ()
+{
+  return PosZ;
+}
+
+}
---
+++ Assets/Scripts/ShootControl.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ShootControl : MonoBehaviour
+{
+
+protected float Animation;
+protected GameObject GoalLine;
+protected AimingControl AimCtrl;
+
+protected float posX = 0.0f;
+protected float posY = 0.0f;
+protected float posZ = 0.0f;
+
+void start()
+{
+  // GoalLine = GameObject.Find("GoalLine");
+  // AimCtrl = GoalLine.GetComponent<AimingControl>();
+}
+
+void FixedUpdate()
+{
+
+}
+
+void OnMouseDown()
+{
+  GameObject goalline = GameObject.Find("GoalLine");
+  AimingControl aiming = goalline.GetComponent<AimingControl>();
+  Debug.Log("aiming posX: " + aiming.getPosX() + " | aiming posY: " + aiming.getPosY() + " | aiming posZ: " + aiming.getPosZ());
+  
+  float posX = aiming.getPosX();
+  float posY = aiming.getPosY();
+  float posZ = aiming.getPosZ();
+
+  Animation += Time.fixedDeltaTime;
+  Animation = Animation % 5f;
+  // transform.position = MathParabola.Parabola(Vector3.zero, Vector3.forward *10f, 5f, Animation / 5f);
+
+  transform.position = new Vector3(posX, posY, 2.5f);
+  // Debug.Log("posX: " + this.posX + " | posY: " + this.posY);
+
+  // Debug.Log("posX: " + AimCtrl.getPosX() + " | posY: " + AimCtrl.getPosY());
+
+}
+
+}
