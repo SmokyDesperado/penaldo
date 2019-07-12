03793457c16173eda5d1496d7cadc4013ac83bae
SmokyDesperado <desperadosmoky@gmail.com>
2019-07-12 15:53:46 +0200

    shoot updated
    shooting behavior updated

--- Assets/Scripts/ShootControl.cs
+++ Assets/Scripts/ShootControl.cs
@@ -20,19 +20,23 @@ public float StartPosZ = -3.635868f;
 public float RespawnTimeInSeconds = 1f;
 public float shootForce = 10f;
 
-public float shootForceX = 10f;
+public float shootForceX = 5f;
 public float shootForceY = 10f;
+
+private float goalLineYModifier = 1.7f;
 
 void start()
 {
-  // GoalLine = GameObject.Find("GoalLine");
-  // AimCtrl = GoalLine.GetComponent<AimingControl>();
   ResetStartingPosition();
 }
 
 private void ResetStartingPosition()
 {
   Debug.Log("ResetStartingPosition");
+
+  GetComponent<Rigidbody>().velocity = new Vector3 (0,0,0);
+  GetComponent<Rigidbody>().angularVelocity = new Vector3 (0,0,0);
+  
   transform.position = new Vector3(StartPosX, StartPosY, StartPosZ);
 }
 
@@ -45,20 +49,16 @@ void OnMouseDown()
 {
   GameObject goalline = GameObject.Find("GoalLine");
   AimingControl aiming = goalline.GetComponent<AimingControl>();
-  // Debug.Log("aiming posX: " + aiming.getPosX() + " | aiming posY: " + aiming.getPosY() + " | aiming posZ: " + aiming.getPosZ());
-  // Debug.Log("posX: " + this.posX + " | posY: " + this.posY);
-
+  
   float posX = (StartPosX - aiming.getPosX()) * shootForceX;
-  float posY = (StartPosY - aiming.getPosY()) * shootForceY;
+  float posY = StartPosY + ((goalLineYModifier - aiming.getPosY()) * shootForceY);
   float posZ = aiming.getPosZ() - StartPosZ;
 
-  Debug.Log("posX: " + posX + " | posY: " + posY + " | posZ: " + posZ);
+  // Debug.Log("posX: " + posX + " | posY: " + posY + " | posZ: " + posZ);
 
   Animation += Time.fixedDeltaTime;
   Animation = Animation % 5f;
-  // transform.position = MathParabola.Parabola(Vector3.zero, Vector3.forward *10f, 5f, Animation / 5f);
 
-  // transform.position = new Vector3(posX, posY, 2.5f);
   GetComponent<Rigidbody>().AddForce(posX, posY, shootForce, ForceMode.Impulse);
 
   Invoke("ResetStartingPosition", RespawnTimeInSeconds);
